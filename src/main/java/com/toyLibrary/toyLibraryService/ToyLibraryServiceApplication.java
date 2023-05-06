65f8f95edaeaf9c02738283edad2d9040309fa5c
umawani <umawani@student.unimelb.edu.au>
2023-05-06 16:09:46 +0500

    Initial Setup

---
+++ src/main/java/com/toyLibrary/toyLibraryService/ToyLibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.toyLibrary.toyLibraryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyLibraryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyLibraryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyLibrary/toyLibraryService/ToyLibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyLibrary.toyLibraryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyLibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
